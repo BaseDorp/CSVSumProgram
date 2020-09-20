9370386a3c6b90ecd07ceef491d16565d73b133c
BaseDorp <44095612+BaseDorp@users.noreply.github.com>
2020-09-20 00:36:31 -0500

    Initial Commit

---
+++ CSVSumAverage/CSVSumAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSVSumAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Directory.GetCurrentDirectory();
+            // get the current filepath
+            // TODO create a folder for the csv files if there is not one already
+            // TODO throw error if no CSV files are in that folder
+            Console.WriteLine(filePath);
+
+            List<string> lines = new List<string>();
+
+            Console.WriteLine("Press Any Key to Continue ...");
+            Console.ReadKey();
+        }
+    }
+}
